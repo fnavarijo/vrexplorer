689a296d753c2630821ba12e5db33cce3fb9cce1
Fernando Navarijo <fnavarijo@pop-os.localdomain>
2020-05-26 23:39:36 -0600

    Updating lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,17 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    'nuxt/no-cjs-in-config': 'off'
+    // eslint
+    'nuxt/no-cjs-in-config': 'off',
+    'space-before-function-paren': ['error', 'never'],
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'never',
+      'exports': 'never',
+      'functions': 'never'
+    }],
+    // ts-lint
+    '@typescript-eslint/explicit-function-return-type': ['error'],
   }
 }
